aae565caecccb293c2c2a3d73093683cab665ee4
asuselessbrain <shovon1023456789@gmail.com>
2024-07-09 17:03:07 +0600

    mongodb connected successfully

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const { MongoClient } = require("mongodb");
+const app = express()
+const port = process.env.PORT || 3000
+
+
+app.use(cors())
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.x6ipdw6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const client = new MongoClient(uri);
+async function run() {
+  try {
+    const users = client.db('bestroBoss').collection('users');
+    // Query for a movie that has the title 'Back to the Future'
+
+    app.get('/users', async(req, res) =>{
+        const result = await users.find().toArray()
+        res.send(result)
+    })
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
