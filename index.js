bbfe514311e97f7335b074cce47e93d70f286c07
asuselessbrain <shovon1023456789@gmail.com>
2024-07-09 19:36:09 +0600

    user info insert in database api is created

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 const { MongoClient } = require("mongodb");
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
-
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.x6ipdw6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.x6ipdw6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const client = new MongoClient(uri);
 async function run() {
   try {
-    const users = client.db('bestroBoss').collection('users');
+    const user = client.db("bestroBoss").collection("users");
+    const usersCollection = client.db("astraGadgets").collection("users");
     // Query for a movie that has the title 'Back to the Future'
 
-    app.get('/users', async(req, res) =>{
-        const result = await users.find().toArray()
-        res.send(result)
-    })
+    app.get("/user", async (req, res) => {
+      const result = await user.find().toArray();
+      res.send(result);
+    });
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -27,10 +32,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
